004a30b3b68f496a0e4cee90d5db30ea9b48cc9e
mallorymondays <mallory.willyerd@gmail.com>
2021-04-27 17:43:18 -0700

    assignment week 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let myYear = new Date();
 let myDate =myYear.getFullYear();
 document.getElementById ("year").textContent= myDate;
-document.getElementById ("lu").textContent="Last Updated:";
+document.getElementById ("lu").textContent="Last Updated:" + document.lastModified;
