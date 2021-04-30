ae4bec4f0d9b2df18ac996488a3a9a05e98d3d14
mallorymondays <mallory.willyerd@gmail.com>
2021-04-29 17:49:25 -0700

    update

---
+++ lesson 2/js/script.js
@@ -0,0 +1,5 @@
+let myYear = new Date();
+let myDate =myYear.getFullYear();
+
+document.getElementById ("year").textContent= myDate;
+document.getElementById ("lu").textContent="Last Updated: " + document.lastModified;
