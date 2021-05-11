a4c75f6d71c931293bc3f6963208ee67e2e7eb07
mallorymondays <mallory.willyerd@gmail.com>
2021-05-11 16:05:26 -0700

    update

--- lesson4/js/footerscript.js
+++ lesson4/js/footerscript.js
@@ -3,4 +3,4 @@ let myDate = myYear.getFullYear();
 
 document.getElementById("year").textContent = myDate;
 document.getElementById("lu").textContent =
-  "Last Updated: " + document.lastModified;
+  "  Last Updated: " + document.lastModified;
