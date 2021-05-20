cac9c638caa7903997188fd970191f36b0ac9c9d
mallorymondays <mallory.willyerd@gmail.com>
2021-05-19 17:12:33 -0700

    update

--- bom/js/script.js
+++ bom/js/script.js
@@ -1,5 +1,22 @@
-let myYear = new Date();
-let myDate =myYear.getFullYear();
-
-document.getElementById ("year").textContent= myDate;
-document.getElementById ("lu").textContent="Last Updated: " + document.lastModified;
+const input = document.querySelector('input');
+input.focus();
+const addChapterButton = document.getElementById('addchapter');
+const list = document.getElementById('list');
+addChapterButton.addEventListener('click', function () {
+    if (input != null) {
+        const listItem = document.createElement('li');
+        listItem.textContent = document.getElementById('chapter').value;
+        document.getElementById('chapter'). value = "";
+        // input.textContent = "";
+        const listButton = document.createElement('button');
+        listButton.textContent = "❌";
+        // delete here
+        listButton.addEventListener('click', function () {
+            listItem.remove();
+            input.focus();
+        });
+        listItem.append(listButton);
+        list.appendChild(listItem);
+    }
+});
+// document.getElementById('chapter').Value
