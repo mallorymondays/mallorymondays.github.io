93d02330f37b9df89c6dcab6fa38738ebcaca8ba
mallorymondays <mallory.willyerd@gmail.com>
2021-06-02 20:03:48 -0700

    update

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,11 +1,14 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
-
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
+};
   if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
