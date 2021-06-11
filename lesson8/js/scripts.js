98a4e4536f52c5c23179cc82d94d292c718b4002
mallorymondays <mallory.willyerd@gmail.com>
2021-06-10 22:14:49 -0700

    update

--- lesson8/js/scripts.js
+++ lesson8/js/scripts.js
@@ -2,5 +2,7 @@ function toggleMenu (){
 document.getElementById("primNav").classList.toggle("hide");
 }
 
-
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
 
