98458bda5f284e26b0970373396605ca93304f77
mallorymondays <mallory.willyerd@gmail.com>
2021-06-28 20:21:27 -0700

    update

--- lesson11/js/sodasprings.js
+++ lesson11/js/sodasprings.js
@@ -39,6 +39,7 @@ console.log (mylist);
         let iconpath = "//openweathermap.org/img/w/" + iconcode + ".png";
         let theIcon= document.createElement("img");
         theIcon.src = iconpath;
+        theIcon.alt = mylist[i].weather[0].description;
 
         let theDay = document.createElement("div");
         theDay.appendChild(theDayname);
