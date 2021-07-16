39f98cf9dddc1e4372051b20a231afb708feb34b
mallorymondays <mallory.willyerd@gmail.com>
2021-07-16 16:04:01 -0700

    update

--- final/js/directory.js
+++ final/js/directory.js
@@ -1,40 +1,42 @@
+
+
 const requestURL = "https://mallorymondays.github.io/directory.json";
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
-  .then(function (jsonObject) {
-    const directory = jsonObject["directory"];
-    console.log (directory);
-
-    for (let i = 0; i < directory.length; i++) {
-      if (i == 0 || i == 1 || i == 2) {
-        let card = document.createElement("div");
-        let info = document.createElement("section");
-        let image = document.createElement("figure");
-
-        let name = document.createElement("h3");
-        let website = document.createElement("p");
-        let photo = document.createElement("img");
-
-        name.textContent = directory[i].name;
-        website.textContent = directory[i].website;
-        
-
-        photo.setAttribute("src", "images/" + directory[i].photo);
-        photo.setAttribute("alt", directory[i].name);
-        photo.setAttribute("width", "200");
-        photo.setAttribute("height", "200");
-
-        info.appendChild(name);
-        info.appendChild(website);
-        
-
-        card.appendChild(info);
-        card.appendChild(image);
-
-        document.getElementById("gallery").appendChild(card);
+  .then(function (directoryCard) {
+    const directory = directoryCard["directory"];
+      
+       for (let i = 0; i < directory.length; i++) {
+        if (i == 0 || i == 1 || i == 2) {
+          let card = document.createElement("div");
+          let info = document.createElement("section");
+          let image = document.createElement("figure");
+  
+          let name = document.createElement("h2");
+          let website = document.createElement("p");
+          let photo = document.createElement("img");
+  
+          name.textContent = directory[i].name;
+          website.textContent = directory[i].website;
+          
+  
+          photo.setAttribute("src", "images/" + directory[i].photo);
+          photo.setAttribute("alt", directory[i].name);
+          photo.setAttribute("width", "200");
+          photo.setAttribute("height", "200");
+  
+          info.appendChild(name);
+          info.appendChild(website);
+          
+  
+          card.appendChild(info);
+          image.appendChild(photo);
+          card.appendChild(image);
+  
+          document.getElementById("directorycards").appendChild(card);
+        }
       }
-    }
-  });
+    });
