49adf69e2b4b3fed6723de8faea6bddbfe5e4df5
mallorymondays <mallory.willyerd@gmail.com>
2021-07-20 16:23:00 -0700

    update

--- final/js/mesa.js
+++ final/js/mesa.js
@@ -1,6 +1,5 @@
-
-const todayDayNumber = today.getDay();
-
+const todayDay = today.getDay();
+const todayDate = new Date();
 const myweekday = new Array(7);
 myweekday[0] = "Sunday";
 myweekday[1] = "Monday";
@@ -11,50 +10,42 @@ myweekday[5] = "Friday";
 myweekday[6] = "Saturday";
 
 const apiURL =
-  "//api.openweathermap.org/data/2.5/forecast?q=mesa,az,US&appid=84297c2a0a0b88e4f791efca7bb46439&units=imperial";
+  "//api.openweathermap.org/data/2.5/onecall?lat=33.438150&lon=-111.687160&exclude=hourly,minutely&appid=84297c2a0a0b88e4f791efca7bb46439&units=imperial";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((weatherInfo) => {
-    
+    console.log(weatherInfo);
 
-    let mylist = weatherInfo.list;
-    let forecastDayNumber = todayDayNumber;
+    let mylist = weatherInfo.daily;
+    let forecastDay = todayDay;
 
-
-    for (i = 0; i < mylist.length; i++) {
-      let time = mylist[i].dt_txt;
-      if (time.includes("21:00:00"))
-      {
-        forecastDayNumber += 1;
-      if (forecastDayNumber === 7) {
-        forecastDayNumber = 0;
+    for (i = 0; i < 3; i++) {
+      forecastDay += 1;
+      if (forecastDay === 7) {
+        forecastDay = 0;
       }
-        let theDayname = document.createElement("h5");
-        theDayname.textContent = myweekday[forecastDayNumber];
+      let theDayname = document.createElement("h5");
+      theDayname.textContent = myweekday[forecastDay];
 
-        let theTemp = document.createElement("p");
-        theTemp.textContent = Math.round (mylist[i].main.temp) + "\xB0";
+      let theTemp = document.createElement("p");
+      theTemp.textContent = Math.round(weatherInfo.current.temp)
+      +"\xB0";
 
-        let iconcode = mylist[i].weather[0].icon;
-        let iconpath = "//openweathermap.org/img/w/" + iconcode + ".png";
-        let theIcon= document.createElement("img");
-        theIcon.src = iconpath;
-        theIcon.alt = mylist[i].weather[0].description;
+      let iconcode = mylist[i].weather[0].icon;
+      let iconpath = "//openweathermap.org/img/w/" + iconcode + ".png";
+      let theIcon = document.createElement("img");
+      theIcon.src = iconpath;
+      theIcon.alt = mylist[i].weather[0].description;
 
-        let theDay = document.createElement("div");
-        theDay.appendChild(theDayname);
-        theDay.appendChild(theTemp);
-        theDay.appendChild(theIcon);
+      let theDay = document.createElement("div");
+      theDay.appendChild(theDayname);
+      theDay.appendChild(theTemp);
+      theDay.appendChild(theIcon);
 
-        
-
-        document.getElementById("weatherforecast").appendChild(theDay);
-
-      } 
-      
-    } 
-  }); 
+      document.getElementById("weatherforecast").appendChild(theDay);
+    }
+  });
 
 //weather summary
 
@@ -63,17 +54,11 @@ const currentapiURL =
 fetch(currentapiURL)
   .then((response) => response.json())
   .then((weatherInfo) => {
-   
-
     document.getElementById("currentTemp").innerHTML = Math.round(
       weatherInfo.current.temp
     );
-        document.getElementById("description").innerHTML =
+    document.getElementById("description").innerHTML =
       weatherInfo.current.weather[0].description;
-    document.getElementById("humidity").innerHTML = weatherInfo.current.humidity;
-
-   
+    document.getElementById("humidity").innerHTML =
+      weatherInfo.current.humidity;
   });
-
-
- 
